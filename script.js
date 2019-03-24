0a6169df8cbd5026373186c00efe740adda6d0d0
Prashant1304 <sinha.prashant134@gmail.com>
2019-03-25 01:50:32 +0530

    My Commit

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-/*var noofrows = 5;
-var rowCount = 1;
-        for (var i = noofrows; i > 0; i--)
-           {
-            for (var j = 1; j <= rowCount; j++)
-            {
-                document.write(rowCount+' ');
-            }
-               document.write("<br/>");
-            rowCount++;
-        } */
-/*var noofrows = 5;
-var rowCount = 1;
-        for (var i = noofrows; i > 0; i--)
-        {
-            for (var j = 1; j <= i; j++)
-            {
-                document.write(" ");
-            }
-        
-           
-            for (var j = 1; j <= rowCount; j++)
-            {
-                document.write(rowCount+' ');
-            }
-            document.
-               document.write("<br/>");
-            rowCount++;
-        }*/
         for (var i = 1; i < 6; i++)
            { 
                for(var k=1; k <=(5-i); k++)
